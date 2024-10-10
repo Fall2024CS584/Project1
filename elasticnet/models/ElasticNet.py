818866f5d281321f9fdb83e5832e8fe6fd6f95b8
Haeun Suh <hsuh7@hawk.iit.edu>
2024-10-10 00:05:39 -0500

    First draft

--- elasticnet/models/ElasticNet.py
+++ elasticnet/models/ElasticNet.py
@@ -1,17 +1,103 @@
+# Algorithm to submit as Project1
+import numpy as np
 
 
-class ElasticNetModel():
-    def __init__(self):
-        pass
+# 완성 예정본
+class ElasticNetModel:
+    def __init__(self, learning_rate: float, epochs: int, alpha: float, rho: float, optimization=False):
+        self.ns = None  # Number of samples
+        self.nf = None  # Number of features
+        self.X = None  # Training Set - Parameters
+        self.Y = None  # Training Set - Labels
+        self.lr = learning_rate  # Learning rate
+        self.epochs = epochs  # Number of Iteration
+        self.l1_lambda = alpha * rho  # λ for L1(LASSO) : λ_1 = α*ρ
+        self.l2_lambda = alpha * (1 - rho)  # λ for L2(RIDGE) : λ_2 = α*(1-ρ)
+        self.w = None  # Init weights(parameters) of the model
+        self.b = 0  # Init bias(parameter) of the model
+        self.cost = -1  # Init cost function of the model
+        self.optimization = optimization  # Whether to find optimal cost option
+        self.models = []  # List to store train results
+        self.epoch = epochs  # Best epoch for the best fit model
+
+    def fit(self, x: np.ndarray, y: np.ndarray) -> object:
+        """
+        Train to build a predictive model
+        :param x: input training data
+        :param y: target training data
+        :return: class ElasticNetModelResults object for prediction
+        """
+        # Preprocess the data
+        self.ns, self.nf = x.shape  # Collect shape
+        self.X = x.astype(np.float64)
+        self.Y = y.astype(np.float64)[:, np.newaxis]
+
+        # Init parameters
+        self.w = np.zeros((self.nf, 1))
+        self.b = 0
+
+        # Training
+        for trial in range(self.epochs):
+            cost = self.update_weights()
+            self.models.append((trial + 1, self.w, self.b, cost))
+
+        return ElasticNetModelResults(self)
+
+    def cost_function(self, y, y_pred):
+        """
+        Computing the cost function of the given weights
+        :param y: Original target value
+        :param y_pred: predicted target value
+        :return computed cost function
+        """
+        l1_penalty = self.l1_lambda * np.sum(np.abs(self.w))
+        l2_penalty = self.l2_lambda * 0.5 * np.sum(np.square(self.w))
+        return 1 / (2 * self.ns) * np.sum(np.square(y_pred - y)) + (l1_penalty + l2_penalty)
+
+    def update_weights(self) -> float:
+        """
+
+        :return: cost function of the current trial
+        """
+        # Here, self.weights acts like column vector as (n_features, ) for the computation purpose
+        Y_pred = self.X.dot(self.w) + self.b
+        residuals = self.Y - Y_pred
+        dW = np.zeros((self.nf, 1))
+
+        # should collect the derivation due to L1
+        for j in range(self.nf):
+            MSE = - np.dot(self.X[:, j], residuals) / self.ns
+            L1 = self.l1_lambda * np.sign(self.w[j])  # L1 penalty
+            L2 = self.l2_lambda * self.w[j]  # L2 penalty
+
+            # Update the gradient with MSE, L1, and L2
+            dW[j] = MSE + L1 + L2
+
+        # get bias
+        db = - np.sum(residuals) / self.ns
+
+        # Apply weight&bias updates
+        self.w = self.w - self.lr * dW
+        self.b = self.b - self.lr * db
+
+        # Apply soft thresholding after weight update
+        for j in range(self.nf):
+            self.w[j] = np.sign(self.w[j]) * np.maximum(np.abs(self.w[j]) - self.l1_lambda, 0)
+
+        return self.cost_function(self.Y, Y_pred)
 
 
-    def fit(self, X, y):
-        return ElasticNetModelResults()
+class ElasticNetModelResults:
+    def __init__(self, model: ElasticNetModel):
+        self.model = model  # trained model
+        if self.model.optimization:  # If apply optimization
+            self.epoch, self.model.w, self.model.b, self.cost = min(self.model.models, key=lambda a: a[3])
 
-
-class ElasticNetModelResults():
-    def __init__(self):
-        pass
-
-    def predict(self, x):
-        return 0.5
+    def predict(self, x: np.ndarray) -> np.ndarray:
+        """
+        Predict the output of the model with respect to the input data and return the predicted values
+        :param x: Input data to test the model
+        :return: Predicted values for the input data
+        """
+        x = x.astype(np.float64)  # Preprocessing the data
+        return np.dot(x, self.model.w) + self.model.b
--- elasticnet/tests/test_ElasticNetModel.py
+++ elasticnet/tests/test_ElasticNetModel.py
@@ -1,19 +1,102 @@
-import csv
-
-import numpy
-
+# import csv
+import numpy as np
 from elasticnet.models.ElasticNet import ElasticNetModel
 
-def test_predict():
-    model = ElasticNetModel()
-    data = []
-    with open("small_test.csv", "r") as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            data.append(row)
 
-    X = numpy.array([[v for k,v in datum.items() if k.startswith('x')] for datum in data])
-    y = numpy.array([[v for k,v in datum.items() if k=='y'] for datum in data])
-    results = model.fit(X,y)
-    preds = results.predict(X)
-    assert preds == 0.5
+def generate_synthetic_data(n_samples=50, n_features=3, weights=None, noise_std=0.1, random_state=None, test_ratio=0.2):
+    """
+    n_samples: Number of samples to generate
+    n_features: Number of features to generate
+    weights: predefined weights - Create a random weight if no weights are defined
+    noise_std: noise scale. default is 0
+    random_state: random seed
+    sample_weight: portion of the test data over total samples. default is 0.2
+    """
+    if random_state:
+        np.random.seed(random_state)
+
+    test_size = int(n_samples * test_ratio)
+
+    # random sample
+    x = np.random.rand(n_samples, n_features)
+
+    # create weighted if not specified
+    if weights is None:
+        weights = np.random.randn(n_features)
+
+    # create noise
+    noise = np.random.normal(0, noise_std, size=n_samples)
+
+    # create y
+    y = np.dot(x, weights) + noise
+
+    return x[test_size:], y[test_size:], x[:test_size], y[:test_size], np.array(weights)[:, np.newaxis]
+
+
+def euclidean_distance(actual_weight, predicted_weight):
+    """
+    Calculates the Euclidean distance between two weights to check similarity
+    :param actual_weight: Predefined weights matrix(Or vector)
+    :param predicted_weight: Trained weights matrix(Or vector)
+    :return: Computed distance between two matrices (Near 0 will be good!)
+    """
+    if actual_weight.shape == predicted_weight.shape:
+        return np.sqrt(np.sum((actual_weight - predicted_weight) ** 2))
+
+
+def r2_score(y_true, y_pred):
+    """
+    Calculates the R2 score between two weights to check the validity of the model.
+    :param y_true: Actual weights
+    :param y_pred: Predicted weights
+    :return: R2 score between two weights
+    """
+    # Average of actual weights
+    y_mean = np.mean(y_true)
+
+    # Total Sum of Squares, TSS
+    TSS = np.sum((y_true - y_mean) ** 2)
+
+    # Residual Sum Of Squares, RSS
+    RSS = np.sum((y_true - y_pred) ** 2)
+
+    # Compute R2
+    r2 = 1 - (RSS / TSS)
+
+    return r2
+
+
+def test_predict():
+    # Test data 1:  small_test.csv
+    # data = []
+    # with open("small_test.csv", "r") as file:
+    #     reader = csv.DictReader(file)
+    #     for row in reader:
+    #         data.append(row)
+    #
+    # train_x = np.array([[v for k, v in datum.items() if k.startswith('x')] for datum in data])
+    # train_x = np.array([[v for k, v in datum.items() if k == 'y'] for datum in data])
+    # test_x = train_x # Set as the same
+    # test_y = train_y # Set as the same
+
+    # Test data 2:  Random generation
+    # n_samples=50, n_features=3, weights=random, noise_std=0.0
+    train_x, train_y, test_x, test_y, true_weights = generate_synthetic_data(n_samples=50, n_features=3)
+
+    # define the parameters
+    param = {
+        "learning_rate": 0.01,
+        "epochs": 1000,
+        "alpha": 0.001,
+        "rho": 0.015,
+        "optimization": True
+    }
+    model = ElasticNetModel(**param)
+
+    results = model.fit(train_x, train_y)
+    predicts = results.predict(test_x)
+    print(predicts)
+    print("=" * 100)
+    print(f"R2 score of the model is {r2_score(test_y, predicts)}")
+    print("Number of the best epochs: ", results.epoch)
+    print("Cost of the best epochs: ", results.cost)
