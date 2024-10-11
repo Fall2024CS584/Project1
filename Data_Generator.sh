45672a0e48af3cb186fb86d8d4e6359eb4ec7955
Jerry Ou <Jerryou_cn@outlook.com>
2024-10-10 22:49:11 -0500

    update from ZIRUI OU

---
+++ Data_Generator.sh
@@ -0,0 +1,2 @@
+python generate_regression_data.py -N 100 -m 2.5 1.5 -b 0.5 -scale 1.0 -rnge -10 10 -seed 42 -output_file train_data.csv
+python generate_regression_data.py -N 30 -m 2.5 1.5 -b 0.5 -scale 1.0 -rnge -10 10 -seed 42 -output_file test_data.csv
--- elasticnet/tests/test_ElasticNetModel.py
+++ elasticnet/tests/test_ElasticNetModel.py
@@ -1,19 +1,45 @@
 import csv
+import numpy as np
+import sys
+import os
 
-import numpy
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.dirname(os.path.dirname(current_dir))
+sys.path.append(project_root)
 
 from elasticnet.models.ElasticNet import ElasticNetModel
 
-def test_predict():
-    model = ElasticNetModel()
+def load_data(filepath):
     data = []
-    with open("small_test.csv", "r") as file:
+    with open(filepath, "r") as file:
         reader = csv.DictReader(file)
         for row in reader:
             data.append(row)
+    X = np.array([[float(v) for k, v in datum.items() if k.startswith('x')] for datum in data])
+    y = np.array([float(datum['y']) for datum in data])
+    return X, y
 
-    X = numpy.array([[v for k,v in datum.items() if k.startswith('x')] for datum in data])
-    y = numpy.array([[v for k,v in datum.items() if k=='y'] for datum in data])
-    results = model.fit(X,y)
-    preds = results.predict(X)
-    assert preds == 0.5
+def test_predict():
+    model = ElasticNetModel()
+
+    train_X, train_y = load_data(os.path.join(project_root, 'train_data.csv'))
+
+    test_X, test_y = load_data(os.path.join(project_root, 'test_data.csv'))
+
+    model.fit(train_X, train_y)
+
+    preds = model.predict(test_X)
+    #print(f"prediction:\n {preds}")
+
+    mse = np.mean((preds - test_y) ** 2)
+    #print(mse)
+    assert mse < 1
+
+    print("Actual\tPredicted\tAbs Error")
+    
+    [print(f"{a}\t{p}\t{abs(a-p)}") for a, p in zip(test_y, preds)]
+
+    print(f"MSE : {mse}")
+
+if __name__ == "__main__":
+    test_predict()
