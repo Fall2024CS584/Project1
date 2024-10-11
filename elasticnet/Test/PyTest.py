96469b4e05ad90b11b9195ea100bd24fdb229b97
Atharv Manoj Patil <93934533+AtharvPat@users.noreply.github.com>
2024-10-10 22:25:44 -0500

    Add files via upload

---
+++ elasticnet/Test/PyTest.py
@@ -0,0 +1,98 @@
+import pytest
+import numpy as np
+import pandas as pd
+import sys 
+
+''' give your root directory path here'''
+sys.path.insert(1,"----Give your root directory path----")
+
+from Models.ElasticNet import ElasticNetModel
+from Models.ElasticNet import ElasticNetModelResults
+from Models.Data_Gen import DataGenerator
+from Models.Data_Gen import ProfessorData
+from Test.Test_Model import *
+
+
+@pytest.fixture
+def generate_data():
+    """Fixture to generate data."""
+    data_gen = DataGenerator(rows= 1000, cols= 10, noise= 0.3, seed= 10)
+    X, y = data_gen.gen_data()
+    return X, y
+
+
+def test_data_generation(generate_data):
+    """Test if data generation returns the correct shapes."""
+    X, y =generate_data
+    assert X.shape == (1000, 10), f"Expected X shape (1000, 10), but got {X.shape} "
+    assert y.shape == (1000,), f"Expected y shape (1000,), but got {y.shape}"
+
+
+def test_train_test_split( generate_data):
+    """Test train-test split testing."""
+    X, y = generate_data
+    X_train,X_test, y_train,y_test = train_test_split(X, y, test_size=0.2)
+
+    assert X_train.shape[0] == 800, f"Expected 80 training samples, but got {X_train.shape[0]}"
+    assert X_test.shape[0] == 200, f"Expected 20 test samples, but got {X_test.shape[0]}"
+    assert y_train.shape[0] == 800, f"Expected 80 training labels, but got {y_train.shape[0]}"
+    assert y_test.shape[0] == 200, f"Expected 20 test labels, but got {y_test.shape[0]}"
+
+
+def test_standardize(generate_data):
+    """Test standardize"""
+    X, _ = generate_data
+    X_train, X_test, _, _ = train_test_split(X, np.arange(1000), test_size=0.2)
+
+    X_train_std, X_test_std = standardize(X_train, X_test)
+    
+    assert np.allclose(X_train_std.mean(axis=0), 0, atol=1e-7), "Expected mean close to 0 after standardization for each column"
+    assert np.allclose(X_train_std.std(axis=0), 1, atol=1e-7), "Expected std close to 1 after standardization for each column"
+
+
+
+@pytest.fixture
+def elasticnet_model():
+
+    return ElasticNetModel(alpha=0.01,penalty_ratio=0.1,learning_rate=0.001,  iterations=1000)
+
+
+def test_elasticnet_model_fit_predict(generate_data, elasticnet_model):
+    """Test ElasticNetModel fitting and prediction."""
+    X, y = generate_data
+    X_train,X_test,  y_train,y_test = train_test_split(X, y,test_size=0.2)
+    X_train_std,X_test_std = standardize(X_train,  X_test)
+    
+    elasticnet_model.fit(X_train_std.values,  y_train.values)
+    
+    y_pred = elasticnet_model.predict(X_test_std.values)
+    
+    assert y_pred.shape == y_test.shape, "   Predictions matchs the shape of y_test"
+
+
+def test_metrics(generate_data,  elasticnet_model):
+    """Test MSE, MAE, and R2 metrics."""
+    X, y = generate_data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    X_train_std, X_test_std = standardize(X_train, X_test)
+    
+    elasticnet_model.fit( X_train_std.values,y_train.values)
+    y_pred = elasticnet_model.predict(  X_test_std.values)
+    
+    comparison_df = pd.DataFrame({
+        'Actual Values': y_test.values,
+        'Predicted Values': y_pred
+    })
+    comparison_df["Difference"] = comparison_df['Actual Values'] - comparison_df['Predicted Values']
+    
+    mse = np.square(comparison_df["Difference"]).mean()
+    assert mse >= 0, "MSE should be a non-negative value"
+
+    mae = np.abs(comparison_df['Difference']).mean()
+    assert mae >= 0, "MAE should be a non-negative value"
+
+    results = ElasticNetModelResults(y_test=y_test.values, y_pred=y_pred)
+    r2 = results.r2_score(y_test.values, y_pred)
+    assert -1 <= r2 <= 1, "R² should be between -1 and 1"
+
+
---
+++ elasticnet/Test/Test_Model.py
@@ -0,0 +1,150 @@
+import numpy as np
+import pandas as pd
+import sys 
+import argparse
+''' give your root directory path here'''
+sys.path.insert(1,"----Give your root directory path----")
+
+from  Models.ElasticNet import ElasticNetModel
+from  Models.ElasticNet import ElasticNetModelResults
+from  Models.Data_Gen import DataGenerator
+from  Models.Data_Gen import ProfessorData
+
+def train_test_split(x, y, test_size): 
+    np.random.seed(10)
+    
+    rows = x.shape[0]
+    test = int(rows * test_size)
+    test_index = np.random.choice(rows,test, replace= False)
+    
+    dublicate = np.zeros(rows, dtype=bool)
+    dublicate[test_index] = True
+
+    if isinstance(x, pd.DataFrame):
+        X_train = x[~dublicate].reset_index(drop=True)
+        X_test = x[dublicate].reset_index(drop=True)
+    else:
+        X_train = x[~dublicate]
+        X_test = x[dublicate]
+    
+    if isinstance(y, pd.Series):
+        y_train = y[~dublicate].reset_index(drop=True)
+        y_test = y[dublicate].reset_index(drop=True)
+    else:
+        y_train = y[~dublicate]
+        y_test = y[dublicate]
+    
+    return X_train, X_test, y_train, y_test
+
+def standardize(X_train, X_test): 
+    mean = X_train.mean()
+    standard_deviation= X_train.std()
+    X_train_std = (X_train-mean)/standard_deviation
+    X_test_std  = (X_test-mean)/standard_deviation  
+    return X_train_std, X_test_std
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description= " Train an Elastic Net Model on Generated Data")
+    
+    parser.add_argument('--rows', type=int,  help ="Number of Rows/Samples in Generated Data")
+    parser.add_argument('--cols', type=int,  help ="Number of Columns/features in Generated Data")
+    parser.add_argument('--noise', type=float, help ="Noise Scale in Generated Data")
+    parser.add_argument('--seed', type=int,  help ="Random Seed for Data Generation")
+    
+    # Argument Parser for Professor data 
+    parser.add_argument("-N", type = int, help="Number of samples.")
+    parser.add_argument("-m", nargs='+', type = float, help="Expected regression coefficients")
+    parser.add_argument("-b", type= float, help="Offset")
+    parser.add_argument("-scale", type=float, help="Scale of noise")
+    parser.add_argument("-rnge", nargs=2, type=float,  help="Range of Xs")
+    parser.add_argument("-random_seed", type=int, help="A seed to control randomness")
+   
+    parser.add_argument('--test_size', type=float, default=0.2, help ="Size of Test Set")
+    
+    parser.add_argument('--alpha', type=float, default=0.01, help ="Regularization strength for the ElasticNet Model ")
+    parser.add_argument('--penalty_ratio', type=float, default=0.1, help ="Ratio between lasso(L1) and Ridge(L2) penalties")
+    parser.add_argument('--learning_rate', type=float, default=0.001, help ="Model training Learning Rate")
+    parser.add_argument('--iterations', type=int, default=10000, help ="Iterations for Training the model")
+    
+    parser.add_argument('--csv_file_path', type=str ,help ="Csv file path")
+    parser.add_argument('--target_column', type=str, help ="Name of the target column in the csv file")
+    
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    
+    if args.csv_file_path and args.target_column:
+        df = pd.read_csv(args.csv_file_path)
+        print("Columns in DataFrame:", df.columns.tolist())
+
+        if args.target_column in df.columns:
+            X = df.drop(args.target_column, axis=1)
+            y = df[args.target_column]
+        else:
+            raise KeyError(f"Column '{args.target_column}' not found in DataFrame.")
+
+        
+    elif args.rows and args.cols and args.noise:
+        data_gen = DataGenerator(rows=args.rows, cols=args.cols, noise=args.noise, seed=args.seed)
+        X, y = data_gen.gen_data()
+        
+    else: 
+        data_gen = ProfessorData(rnge=args.rnge, scale=args.scale, m=args.m, b=args.b, N=args.N, random_seed=args.random_seed)
+        X, y = data_gen.linear_data_generator()
+
+    
+    # print(X.shape)
+    # print(y.shape)
+        
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= args.test_size)
+    
+    X_train_std, X_test_std = standardize(X_train, X_test)
+    
+    model = ElasticNetModel(alpha = args.alpha,
+                            penalty_ratio = args.penalty_ratio,
+                            learning_rate = args.learning_rate,
+                            iterations = args.iterations)
+    
+    X_train_std_np = X_train_std.values if isinstance(X_train_std, pd.DataFrame) else X_train_std
+    y_train_np = y_train.values if isinstance(y_train, pd.Series) else y_train
+    X_test_std_np = X_test_std.values if isinstance(X_test_std, pd.DataFrame) else X_test_std
+    y_test_np = y_test.values if isinstance(y_test, pd.Series) else y_test
+    
+    # print(X_train_std_np.shape)
+    # print(y_train.shape)
+    model.fit(X_train_std_np,y_train_np)
+    y_pred = model.predict(X_test_std_np) 
+    # print(y_pred)
+    
+    comparison_df = pd.DataFrame({
+        'Actual Values': y_test_np,
+        'Predicted Values': y_pred
+    })
+
+    comparison_df["Difference"] = comparison_df['Actual Values'] - comparison_df['Predicted Values']
+
+
+    mse = np.square(comparison_df["Difference"]).mean()
+    print(f"Mean Squared Error on Test Set: {mse}")
+
+    mae = np.abs(comparison_df['Difference']).mean()
+    print(f"Mean Absolute Error on Test Set: {mae}")
+    
+    results = ElasticNetModelResults(y_test=y_test, y_pred = y_pred)
+    r2 = results.r2_score(y_test_np, y_pred)
+    print(f"R2 Score on Test Set: {r2}")
+    
+    
+
+    
+
+        
+    
+
+    
+    
+    
+    
+
+    
